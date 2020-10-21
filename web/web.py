05b27a01b1f2d522fdc6816801206472587b31a2
Kris Steinhoff <steinhof@umich.edu>
2020-10-20 22:21:12 -0400

    Revert web.py

--- web/web.py
+++ web/web.py
@@ -5,55 +5,51 @@ import pickle
 
 import pandas as pd
 
+
+# This is the starter code from our webteam.
+# You should feel free to change anything you need to change
+
 from flask_wtf import FlaskForm
-from wtforms import SelectField, FloatField
-from flask import Flask, render_template, request
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, FieldList
+from wtforms.validators import DataRequired
+from flask import Flask, redirect, render_template
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
-# The web team will make this more secure in production.
-app.config['SECRET_KEY'] = 'key'
-
+app.config['SECRET_KEY'] = 'key'  ## The web team will make this more secure in production
 app.debug = True
 Bootstrap(app)
+#provide a list of form fields that your model will need
+features = {'feature_name':'Displayname', 'feature_2':'Displayname2'}
+class form(FlaskForm):
+    pass
+class feature_form(FlaskForm):
+    """Create temp form."""
+    for i in features:
+        setattr(FlaskForm, i, StringField(features[i],[
+            DataRequired()]))
 
+model = ""
 
-class FeatureForm(FlaskForm):
-    mean_wind = FloatField('Wind Speed (MPH)', default=2)
-    inches_precip = FloatField('Precipitation (inches)', default=0)
-    is_weekday = SelectField(
-        'Day Type',
-        choices=[('weekday', 'Weekday'), ('weekend', 'Weekend')],
-        default='weekday')
-
-
-with open('models/trip_duration_predictor.pkl', 'rb') as stream:
-    model = pickle.load(stream)
-
-
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-
-    form = FeatureForm(request.args, meta={'csrf': False})
-    if form.validate():
+    form = feature_form()
+    if form.validate_on_submit():
         user_submitted_features = {}
         for i in form:
-            if i.name == 'is_weekday':
-                user_submitted_features['is_weekday'] = i.data == 'weekday'
-            else:
-                user_submitted_features[i.name] = i.data
-
-        user_df = pd.DataFrame(data=[user_submitted_features])
-        user_df['is_weekday'] = user_df[
-            'is_weekday'].astype(str).astype('category')
-
-        prediction = round(model.predict(user_df)[0], 1)
-
-    return render_template("form.html", form=form, prediction=prediction)
+            if i.name == 'csrf_token':
+                continue
+            user_submitted_features[i.name] = i.data
+        # You can use the dictionary user_submitted_features and pass to your model.
+        # you should do that here and return a sentence below.
+        return render_template('results.html', msg = "This will be displayed with after a user submits your form, run your model and put something here")
+    return render_template("form.html", form=form)
 
 
 if __name__ == '__main__':
     app.run()
 
-# Image Credit Anja Nachtweide
+
+
+
+#Image Credit Anja Nachtweide
